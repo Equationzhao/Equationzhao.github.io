259206f569c4b97f8fc576c71c8d8ccdb0471ca9
24601 <equationzhao@foxmail.com>
2023-10-03 00:46:43 +0800

    add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import mdx from '@astrojs/mdx';
 import purgecss from 'astro-purgecss';
 import { defineConfig } from 'astro/config';
-
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Equationzhao.github.io',
-	integrations: [mdx(), sitemap(), purgecss()]
+	integrations: [mdx(), sitemap(), purgecss()],
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+	},
 });
